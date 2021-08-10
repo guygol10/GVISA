d26043076ba702fa17d2fc58f2af77432318c7ac
guygol10 <54492823+guygol10@users.noreply.github.com>
2021-08-10 12:52:12 +0300

    Update Home.js

--- src/Components/Pages/HomePage/Home.js
+++ src/Components/Pages/HomePage/Home.js
@@ -1,1 +1,19 @@
+import React from 'react'
+import HeroSection from '../../HeroSection'
+import Pricing from '../../Pricing'
+import { homeObjFour, homeObjOne, homeObjThree, homeObjTwo } from './Data'
+
+const Home = () => {
+    return (
+        <>
+         <HeroSection  {...homeObjOne}/>   
+         <HeroSection  {...homeObjTwo}/>   
+         <HeroSection  {...homeObjThree}/>   
+         <Pricing />
+         <HeroSection  {...homeObjFour}/>   
+        </>
+    )
+}
+
+export default Home
 
